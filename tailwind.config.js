13ccbecc69004af86eb144428de1625b7cb090e6
Rajat-2701 <rajatchugh7898@gmail.com>
2022-08-19 17:02:15 +0530

    second change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  important:true,
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        Montserrat:["Montserrat"] 
+      },
+      boxShadow:{
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      },
+      boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      }
+    },
   },
   plugins: [],
-};
+}
